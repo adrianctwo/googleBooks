a50dbc4321ea534486df9241314f06982cc28331
adrianctwo <adrian.e.situ@gmail.com>
2019-11-15 11:20:42 -0800

    first commit

---
+++ client/src/pages/NoMatch/NoMatch.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+
+const NoMatch = () => (
+  <Container>
+    <Jumbotron>
+      <h1>404 Page Not Found</h1>
+      <h1>
+        <span role="img" aria-label="Face With Rolling Eyes Emoji">
+          No Results Found </span>
+      </h1>
+    </Jumbotron>
+  </Container>
+);
+
+export default NoMatch;
